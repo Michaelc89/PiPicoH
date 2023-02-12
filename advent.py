e546c696cb05af41add5556d05228e17b75113b5
Michaelc89 <michaelchrystal89@gmail.com>
2023-02-12 19:31:10 +0000

    added Buttons if

--- advent.py
+++ advent.py
@@ -8,18 +8,29 @@ button1 = Pin(13, Pin.IN, Pin.PULL_DOWN)
 button2 = Pin(8, Pin.IN, Pin.PULL_DOWN)
 button3 = Pin(3, Pin.IN, Pin.PULL_DOWN)
 
+# Set up our LED names and GPIO pin numbers
+red = Pin(18, Pin.OUT)
+amber = Pin(19, Pin.OUT)
+green = Pin(20, Pin.OUT)
+
 while True: # Loop forever
     
     time.sleep(0.2) # Short Delay
         
-    if button1.value() == 1: # If button 1 is pressed
+    if button1.value() == 1 and button2.value() == 1: # If button 1 and 2 are pressed at the same
+        
+        print("Buttons 1 and 2 pressed")
+        green.value(1) # green LED on
+        red.value(0) # red LED off
+
+    elif button1.value() == 1: # If button 1 is pressed
         
         print("Button 1 pressed")
+        amber.value(1) # amber LED on
+        red.value(0) # red LED off
+
+    else: # If no buttons are being pressed
         
-    if button2.value() == 1: # If button 2 is pressed
-        
-        print("Button 2 pressed")
-        
-    if button3.value() == 1: # If button 3 is pressed
-        
-        print("Button 3 pressed")
+        red.value(1) # red LED on
+        amber.value(0) # amber LED off
+        green.value(0) # green LED off
