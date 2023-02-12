18b749439f2de31b6b8c411edb8df738bb3f4889
Michaelc89 <michaelchrystal89@gmail.com>
2023-02-12 19:39:53 +0000

    added Potentiometer

--- advent.py
+++ advent.py
@@ -1,36 +1,40 @@
 # Imports
-from machine import Pin
+from machine import ADC, Pin
 import time
 
-# Set up our button names and GPIO pin numbers
-# Also set pins as inputs and use pull downs
-button1 = Pin(13, Pin.IN, Pin.PULL_DOWN)
-button2 = Pin(8, Pin.IN, Pin.PULL_DOWN)
-button3 = Pin(3, Pin.IN, Pin.PULL_DOWN)
+# Set up the potentiometer on ADC pin 27
+potentiometer = ADC(Pin(27))
 
-# Set up our LED names and GPIO pin numbers
+# Set up the LED pins
 red = Pin(18, Pin.OUT)
 amber = Pin(19, Pin.OUT)
 green = Pin(20, Pin.OUT)
 
-while True: # Loop forever
+# Create a variable for our reading
+reading = 0
+
+while True: # Run forever
     
-    time.sleep(0.2) # Short Delay
+    reading = potentiometer.read_u16() # Read the potentiometer value and set this as our reading variable value
+    
+    print(reading) # Print the reading
+    
+    time.sleep(0.1) # short delay
+    
+    if reading <= 20000: # If reading is less than or equal to 20000
+         
+        red.value(1) # Red ON
+        amber.value(0)
+        green.value(0)
         
-    if button1.value() == 1 and button2.value() == 1: # If button 1 and 2 are pressed at the same
+    elif 20000 < reading < 40000: # If reading is between 20000 and 40000
+    
+        red.value(0) 
+        amber.value(1) # Amber ON
+        green.value(0)
         
-        print("Buttons 1 and 2 pressed")
-        green.value(1) # green LED on
-        red.value(0) # red LED off
-
-    elif button1.value() == 1: # If button 1 is pressed
-        
-        print("Button 1 pressed")
-        amber.value(1) # amber LED on
-        red.value(0) # red LED off
-
-    else: # If no buttons are being pressed
-        
-        red.value(1) # red LED on
-        amber.value(0) # amber LED off
-        green.value(0) # green LED off
+    elif reading >= 40000: # If reading is greater than or equal to 40000
+            
+        red.value(0) 
+        amber.value(0)
+        green.value(1) # Green ON
